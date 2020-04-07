9453db2a8c5c5ade329cf7d393d7dd51f36530f1
Vitor <metroid.p2p@gmail.com>
2020-04-07 11:22:55 -0300

    update

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,17 +5,38 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
+
+    this._listaNegociacoes = new ListaNegociacoes();
+    this._negociacoesView = new NegociacoesView($("#negociacoesView"));
+    this._negociacoesView.update(this._listaNegociacoes);
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView($("#mensagemView"));
+    this._mensagemView.update(this._mensagem);
   }
 
   adiciona(event) {
     event.preventDefault();
 
-    let negociacao = new Negociacao(
-      new Date(this._inputData.value),
+    this._listaNegociacoes.adiciona(this._criaNegociacao());
+    this._mensagem.texto = "Negociação adicionada com sucesso";
+    this._mensagemView.update(this._mensagem);
+    this._negociacoesView.update(this._listaNegociacoes);
+    this._limpaFormulario();
+  }
+
+  _criaNegociacao() {
+    return new Negociacao(
+      DateHelper.textParaData(this._inputData.value),
       this._inputQuantidade.value,
       this._inputValor.value
     );
+  }
 
-    console.log(negociacao)
+  _limpaFormulario() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = "";
+    this._inputValor.value = "";
+
+    this._inputData.focus();
   }
 }
